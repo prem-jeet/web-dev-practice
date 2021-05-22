c03601ccf465f1cc7a414cf1d7f9bc21a8cd0c12
prem-jeet <prem4300795@gmail.com>
2021-05-22 09:55:22 +0530

    changed folder name

---
+++ 1_quiz_card/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    question: "Year of independence?",
+    options: ["1900", "2000", "1947", "1847"],
+    answer: 2,
+  },
+  {
+    question: "Best programming language of 2021?",
+    options: ["Go", "Javascript", "Java", "c++"],
+    answer: 1,
+  },
+  {
+    question: "when did coronavirus Struc?",
+    options: ["1976", "2020", "2019", "2016"],
+    answer: 2,
+  },
+  {
+    question: "National animal of India?",
+    options: ["Elephant", "Eagel", "Tiger", "Peacock"],
+    answer: 2,
+  },
+  {
+    question: "National animal of USA?",
+    options: ["Elephant", "Eagel", "Tiger", "Peacock"],
+    answer: 1,
+  },
+];
+
+export default data;

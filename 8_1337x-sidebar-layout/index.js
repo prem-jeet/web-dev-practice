19575bff0b3c892dc01fbcce1797ace5179bdc02
prem-jeet <62586700+prem-jeet@users.noreply.github.com>
2022-01-30 11:31:38 +0530

    adding 8th layout via file uploda

---
+++ 8_1337x-sidebar-layout/index.js
@@ -0,0 +1,9 @@
+// const heading = document.getElementsByClassName("list-heading")[0];
+// const list_ul = document.getElementsByClassName("list-ul")[0];
+
+// heading.addEventListener("mouseover", () => {
+//   list_ul.style.display = "block";
+// });
+// heading.addEventListener("mouseout", () => {
+//   list_ul.style.display = "none";
+// });
